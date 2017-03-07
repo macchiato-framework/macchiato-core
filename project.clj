ffaca6aacb8ce2dfaf7de788c03b65ce520aa7e8
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-03-07 12:34:03 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/core "0.1.6"
+(defproject macchiato/core "0.1.7"
   :description "core Macchiato HTTP library"
   :url "https://github.com/yogthos/macchiato-framework/macchiato-core"
   :scm {:name "git"
