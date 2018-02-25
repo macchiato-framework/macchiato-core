d5182c333d2fadee1601367d3ae653e1fb3f176d
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2018-02-25 09:42:12 -0500

    bumepd up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/core "0.2.8"
+(defproject macchiato/core "0.2.9"
   :description "core Macchiato HTTP library"
   :url "https://github.com/yogthos/macchiato-framework/macchiato-core"
   :scm {:name "git"
