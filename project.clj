e492eb1e5b67de3fb78a37b39ff5cbd824603c9d
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2023-06-19 18:19:31 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/core "0.2.23"
+(defproject macchiato/core "0.2.24"
   :description "ClojureScript Ring style HTTP server abstraction for Node.js."
   :url "https://github.com/yogthos/macchiato-framework/macchiato-core"
   :scm {:name "git"
